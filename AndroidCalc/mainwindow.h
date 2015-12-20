632d8da25afb90185eb4cdf0ae4a5de7a24b3e05
SquirrelHub <squirrel6946@gmail.com>
2015-12-19 23:45:00 -0600

    What a View!
    Success! His claws came out of the tree with ease! The hopeful squirrel
    continued his climb up. A fear of heights was the last thing on this
    young squirrel's mind. The higher and higher he went, the more massive
    the world appeared to be. After a fair amount of climbing, the young
    squirrel took to a branch that hung just over the tree line of the usual
    roaming grounds. How infinitesimal it all looked to the young squirrel!
    There was so much more to life that the young squirrel never knew!
    Colorful birds, a blue ceiling above him, and even more to explore
    beyond!

--- AndroidCalc/mainwindow.h
+++ AndroidCalc/mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QListWidgetItem>
 namespace Ui {
 class MainWindow;
 }
@@ -15,8 +15,51 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_btnOne_clicked();
+
+    void on_btnTwo_clicked();
+
+    void on_btnThree_clicked();
+
+    void on_btnFour_clicked();
+
+    void on_btnFive_clicked();
+
+    void on_btnSix_clicked();
+
+    void on_btnSeven_clicked();
+
+    void on_btnEight_clicked();
+
+    void on_btnNine_clicked();
+
+    void on_btnZero_clicked();
+
+    void on_btnDot_clicked();
+
+    void on_btnLeftPar_clicked();
+
+    void on_btnRightPar_clicked();
+
+    void on_btnPow_clicked();
+
+    void on_btnDivide_clicked();
+
+    void on_btnMultiply_clicked();
+
+    void on_btnMinus_clicked();
+
+    void on_btnPlus_clicked();
+
+    void on_btnEval_clicked();
+
 private:
     Ui::MainWindow *ui;
+protected:
+    QListWidgetItem *text;
+    QListWidgetItem *result;
+    QList<QString> items;
 };
 
 #endif // MAINWINDOW_H
