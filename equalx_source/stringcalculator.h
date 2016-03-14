d0dec09f65d9cfa0816c98419d2ef0525cde8c22
Crystal <gamewhizzit@gmail.com>
2016-03-14 13:45:24 -0700

    Redid ExpressionElement class to be more versitile.

--- equalx_source/stringcalculator.h
+++ equalx_source/stringcalculator.h
@@ -1,30 +1,11 @@
 #ifndef STRINGCALCULATOR_H
 #define STRINGCALCULATOR_H
 
-#include <QApplication>
-
-#include "expressionelement.h"
-#include "utilities.h"
 
 class StringCalculator
 {
-
-    // Public Variables
 public:
-    static const bool DEBUG = false;
-
-    // Public Methods
-public:
-    static void setup();
-    static int subMain();
-    static double calculateCharVector(QVector<char>);
-    static double subExpressionSolver(QVector<char> vect, int start, int end);
-    static ExpressionElement calculateList(QVector<ExpressionElement>);
-    static QVector<ExpressionElement> generateVector(QVector<char> inputVector);
-    static QVector<char> convertToVector(QString);
-
-private:
-    static bool vectorComparison(QVector<char>, QVector<char>);
+    StringCalculator();
 };
 
 #endif // STRINGCALCULATOR_H
